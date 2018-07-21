639a395e47db8202cdaf1e0d67544eb60a887eb6
ytkwmr <yuta519.ka23tf@docomo.ne.jp>
2018-07-21 15:38:43 +0900

    test

--- myDjango/wsgi.py
+++ myDjango/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
-
+"""
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +14,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myDjango.settings")
 
 application = get_wsgi_application()
+"""
+
+import os
+
+from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myDjango.settings")
+
+application = Cling(get_wsgi_application())
